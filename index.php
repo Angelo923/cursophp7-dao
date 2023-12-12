4dcd586b8574824c0e6b3404b69448479663463d
ANGELO MARCOS SIRINO PEDRO <angelo.pedro@EPM334049.PMPR>
2023-12-12 19:11:27 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -2,15 +2,29 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//CARREGA UM USUÁRIO
+/*$root = new Usuario();
 $root->loadById(3);
-echo $root
+echo $root*/
+
+//CARREGA UMA LISTA DE USUÁRIOS
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
 
 /* NÃO USARÁ MAIS
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);*/ 
+
+//CARREGA UMA LISTA DE USUÁRIOS BUSCANDO PELO LOGIN
+/*$search = Usuario::search("jo");
+echo json_encode($search);*/
+
+//CARREGA UM USUÁRIO USANDO O LOGIN E A SENHA
+$usuario = new Usuario();
+$usuario->login("root","!@#$%");
+
+echo $usuario;
 
 ?>
